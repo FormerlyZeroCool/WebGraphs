4b17bc32225402f8bc93826b934ccac5021b6447
andrewrubinstein <andrew.rubinstein93@gmail.com>
2022-12-14 12:32:11 -0500

    Fix rendering labels for points in cartensian space

--- web/graph.js
+++ web/graph.js
@@ -840,7 +840,7 @@ class Game extends SquareAABBCollidable {
                     const sy = clamp(((y - this.y_min) / this.deltaY) * this.cell_dim[1], 0, this.main_buf.width);
                     const sx = clamp(((x - this.x_min) / this.deltaX) * this.cell_dim[0], 0, this.main_buf.width);
                     //render functions as lines between points in table to buffers
-                    if (sx > last_x && sy !== last_y) {
+                    if (sx > last_x || sy !== last_y) {
                         this.main_buf.ctx.lineTo(sx, sy);
                         last_x = sx;
                         last_y = sy;
@@ -1183,8 +1183,8 @@ class Game extends SquareAABBCollidable {
             screen_y = font_size + 10;
         else if (screen_y > this.main_buf.height)
             screen_y = this.main_buf.height - font_size;
-        ctx.fillText(text, screen_x + dim, screen_y + dim / 2 + offset_y);
         ctx.strokeText(text, screen_x + dim, screen_y + dim / 2 + offset_y);
+        ctx.fillText(text, screen_x + dim, screen_y + dim / 2 + offset_y);
     }
     format_number(value, precision = 2) {
         const dim = 10;
